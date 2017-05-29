d01f4e91d3770d4d7efbd6736e21ab92d2b4d702
pscTheOne <phil@bitcoinsberlin.com>
2017-05-29 20:55:16 +0200

    works now?

--- extras/docker/litecoin/start.sh
+++ extras/docker/litecoin/start.sh
@@ -2,9 +2,8 @@
 
 # Launch, utilizing the SIGTERM/SIGINT propagation pattern from
 # http://veithen.github.io/2014/11/16/sigterm-propagation.html
-: ${PARAMS:=""}
 trap 'kill -TERM $PID' TERM INT
-su litecoin -c /usr/local/bin/litecoind ${PARAMS} $@ &
+su litecoin -c /usr/local/bin/litecoind -datadir=/home/litecoin/.litecoin $@ &
 PID=$!
 wait $PID
 trap - TERM INT
