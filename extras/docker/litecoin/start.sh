5095fa8bfe3ee1a5d3b6a2c11ae145a1687352ed
pscTheOne <phil@bitcoinsberlin.com>
2017-05-29 17:44:37 +0200

    Litecoin-addrindex.

---
+++ extras/docker/litecoin/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Launch, utilizing the SIGTERM/SIGINT propagation pattern from
+# http://veithen.github.io/2014/11/16/sigterm-propagation.html
+: ${PARAMS:=""}
+trap 'kill -TERM $PID' TERM INT
+su litecoin -c /usr/local/bin/litecoind ${PARAMS} $@ &
+PID=$!
+wait $PID
+trap - TERM INT
+wait $PID
+EXIT_STATUS=$?
