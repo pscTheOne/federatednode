4aff7e280e61dd06096ca34c3dabd9b4f10708da
pscTheOne <phil@bitcoinsberlin.com>
2017-06-08 12:45:46 +0200

    fixed startup script.

--- extras/docker/litecoin/start.sh
+++ extras/docker/litecoin/start.sh
@@ -4,7 +4,7 @@
 # http://veithen.github.io/2014/11/16/sigterm-propagation.html
 : ${PARAMS:=""}
 trap 'kill -TERM $PID' TERM INT
-su litecoin -c "/usr/local/bin/litecoind ${PARAMS} $@" &
+su litecoin -c "/usr/local/bin/litecoind --datadir=/home/litecoin/.litecoin ${PARAMS} $@" &
 PID=$!
 wait $PID
 trap - TERM INT
